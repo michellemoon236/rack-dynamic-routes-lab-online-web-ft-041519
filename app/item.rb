4c40fa4c69b230268831af210a4966d3d450414b
Michelle Moon <michellemoon236@gmail.com>
2019-05-27 19:37:47 +0000

    Automatically backed up by Learn

--- app/item.rb
+++ app/item.rb
@@ -1,10 +1,15 @@
 class Item
   attr_accessor :name, :price
 
+  @@items = []
+  
   def initialize(name,price)
     @name = name
     @price = price
+    @@items << self
   end
   
-  def 
+  def self.all 
+    @@items
+  end
 end
