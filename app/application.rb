4bed2a0c9eff257c25b31f9c2b28346f8eb88a6a
Michelle Moon <michellemoon236@gmail.com>
2019-05-27 19:33:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,9 +7,9 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
  
-    if req.path.match(/songs/)
+    if req.path.match(/items/)
  
-      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+      items_name = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
       song = @@songs.find{|s| s.title == song_title}
  
       resp.write song.artist
